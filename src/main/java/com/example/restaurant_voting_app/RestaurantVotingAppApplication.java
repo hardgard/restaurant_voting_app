c87b3856513c84f9cd8baec530adfb6d8fa22ba9
hardgard <akohardgard@gmailcom>
2021-08-02 22:17:37 +0300

    Initial commit

---
+++ src/main/java/com/example/restaurant_voting_app/RestaurantVotingAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.restaurant_voting_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantVotingAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestaurantVotingAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/restaurant_voting_app/RestaurantVotingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restaurant_voting_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantVotingAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
